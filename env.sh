d81bdf8d9d20d899dd14665d0267860af92da734
August <2819763+Dangku@users.noreply.github.com>
2023-02-28 17:15:34 +0800

    update toolchain env.sh
    Signed-off-by: August <2819763+Dangku@users.noreply.github.com>

--- env.sh
+++ env.sh
@@ -1,3 +1,1 @@
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-none-elf-
-export PATH=/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf/bin:/opt/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:/opt/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin:/opt/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin:$PATH
+export PATH=/opt/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin:/opt/gcc-linaro-arm-none-eabi-4.8-2014.04_linux/bin:/opt/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf/bin:$PATH
